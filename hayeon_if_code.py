1de0ecc5c9ef9c6b71e2960738fe7c9d51958115
KangHayeonn <rkdgkdus522@naver.com>
2021-02-08 18:49:25 +0900

    deaf_plus_hayeon
    박지훈은 바보다!!!!!!!!!!!!!

--- hayeon_if_code.py
+++ hayeon_if_code.py
@@ -3,3 +3,8 @@ dict = {'하연':26 , '정준': 25, '지훈': 27, '서영': 23}
 for name,age in dict.items() :
     print('{}의 2021년도 올해 나이는 {}입니다.' .format(name, age))
 print("The end")
+
+
+print('박지훈은 바보다')
+print('박지훈은 바보다')
+print('박지훈은 바보다')
